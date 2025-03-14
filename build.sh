536ad85e793bedf082d935399d089fbf4e4a5b96
RajatJain2003visual <rajatofficial5940@gmail.com>
2025-03-14 12:11:32 +0530

    Modified build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo apt update && sudo apt install -y poppler-utils tesseract-ocr
+apt update && apt install -y poppler-utils tesseract-ocr
+
 pip install -r requirements.txt  # Install Python dependencies
